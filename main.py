55b17087716cf47ef753a2d3d86a4571b4d7b87a
svenjamariko <svenja.taft@icloud.com>
2024-04-15 14:38:30 +0200

    jetz

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ st.write("# EKG APP")
 # Eine Überschrift der zweiten Ebene
 st.write("## Versuchsperson auswählen")
 
-# Eine Auswahlbox
+# Eine Auswahlbox, das Ergebnis wird in current_user gespeichert
 current_user = st.selectbox(
     'Versuchsperson',
+    options = ["Nutzer1", "Nutzer2"], key="sbVersuchsperson")
  
