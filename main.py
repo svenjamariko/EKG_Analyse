32e5d67ba17df5097835b1345910f1e259e59658
svenjamariko <svenja.taft@icloud.com>
2024-04-15 16:21:21 +0200

    safe

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import numpy as np
 import matplotlib as plt
+import read_data as rd
+from PIL import Image
+
+
+person_dict = rd.load_person_data()
+person_names = rd.get_person_list(person_dict)
+print(person_names)
 
 # Eine Überschrift der ersten Ebene
 st.write("# EKG APP")
@@ -8,13 +15,29 @@ st.write("# EKG APP")
 # Eine Überschrift der zweiten Ebene
 st.write("## Versuchsperson auswählen")
 
-# Eine Auswahlbox, das Ergebnis wird in current_user gespeichert
-current_user = st.selectbox(
-    'Versuchsperson',
-    options = ["Nutzer1", "Nutzer2"], key="sbVersuchsperson")
 
 # Session State wird leer angelegt, solange er noch nicht existiert
 if 'current_user' not in st.session_state:
     st.session_state.current_user = 'None'
-st.write("Der Name ist: ", st.session_state.current_user) 
+else:
+    st.write("Der Name ist: ", st.session_state.current_user) 
+
+
+# Eine Auswahlbox, das Ergebnis wird in current_user gespeichert
+st.session_state.current_user = st.selectbox(
+    'Versuchsperson',
+    options = person_names, key="sbVersuchsperson")
+
+current_person = find_person_data_by_name(st.session_state.current_user)
+current_picture_path = current_person["picture_path"]
  
+
+# Laden eines Bilds
+image = Image.open(current_picture_path)
+# Anzeigen eines Bilds mit Caption
+st.image(image, caption=st.session_state.current_user)
+
+if __name__ == '__main__':
+    current_person = find_person_data_by_name(st.session_state.current_user)
+    current_picture_path = current_person["picture_path"]
+    print(current_picture_path)
--- read_data.py
+++ read_data.py
@@ -5,3 +5,32 @@ def load_person_data():
     file = open("data/person_db.json")
     person_data = json.load(file)
     return person_data
+
+def get_person_list(person_data):
+    list_of_names = []
+
+    for eintrag in person_data:
+        list_of_names.append(eintrag["lastname"] + ", " +  eintrag["firstname"])
+    return list_of_names
+
+
+def find_person_data_by_name(suchstring):
+
+    person_data = load_person_data()
+
+    if suchstring == "None":
+        return {}
+    
+    two_names = suchstring.split(", ")
+    vorname = two_names[1]
+    nachname = two_names[0]
+
+    for eintrag in person_data:
+        #print(eintrag)
+        if (eintrag["lastname"] == nachname and eintrag["firstname"] == vorname):
+            return eintrag
+    return {}
+
+
+
+
